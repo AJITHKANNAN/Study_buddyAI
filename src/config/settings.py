76c606e1700b346c0f91bab5077963377a158d5a
Ajith-aaum <ajith.kannan@aaumanalytics.com>
2025-08-22 21:33:08 +0530

    Project Structure

---
+++ src/config/settings.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings():
+
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+    MODEL_NAME = "llama-3.3-70b-versatile"
+    
+    TEMPERATURE = 0.9
+
+    MAX_RETRIES = 3
+
+
+settings = Settings()  
